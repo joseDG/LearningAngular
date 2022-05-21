26da7471d1bb66dd917e62c3e80839136dbcc710
JoseDiazG <josethp@hotmail.com>
2022-05-21 10:58:17 -0500

    Repitiendo la seccion

--- 2-IntroduccionAngular/bases/src/app/app.module.ts
+++ 2-IntroduccionAngular/bases/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador/contador.component';
 
 
 @NgModule({
   declarations: [
     
+  
+    ContadorComponent
   ],
   imports: [
     BrowserModule,
