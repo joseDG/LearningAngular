95014ee92183d9d35e54a88746fd341811ddc0bb
JoseDiazG <josethp@hotmail.com>
2022-07-01 20:27:43 -0500

    Finish Crud Heroes

--- 8-HeroesApp-AngularMaterial-FlexLayoutCRUD/HeroesCompleta/src/app/material/material/material.module.ts
+++ 8-HeroesApp-AngularMaterial-FlexLayoutCRUD/HeroesCompleta/src/app/material/material/material.module.ts
@@ -4,13 +4,16 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog'; 
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule } from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 
@@ -30,7 +33,10 @@ import {MatInputModule } from '@angular/material/input';
     MatProgressSpinnerModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatDialogModule
   ]
 })
 export class MaterialModule { }
