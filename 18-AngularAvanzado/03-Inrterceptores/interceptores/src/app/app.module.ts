9be5216966274f27580d05d92bde19ef8a92af0c
joseDG <josethp1@gmail.com>
2022-12-05 08:24:50 -0500

    Finish interceptores

---
+++ 18-AngularAvanzado/03-Inrterceptores/interceptores/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from './interceptors/interceptor.service';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true
+    },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
